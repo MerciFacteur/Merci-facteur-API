3e485a39e25d616d70b27405fe74b0e7188975a9
robin-epicswan <40164896+robin-epicswan@users.noreply.github.com>
2018-10-18 15:40:39 +0200

    Update hash.php

--- hash-secret-key/hash-php/hash.php
+++ hash-secret-key/hash-php/hash.php
@@ -1,7 +1,6 @@
-
-function hashSecretKey($secretKey){
+function hashSecretKey($secretKey, $serviceId){
         $ts = time();
-        $hashed = hash_hmac('sha256', $this->serviceId.$ts, $secretKey, false);
+        $hashed = hash_hmac('sha256', $serviceId.$ts, $secretKey, false);
         
         return array('timestamp'=>$ts, 'hash'=>$hashed);
     }
