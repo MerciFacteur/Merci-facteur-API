e2772db6cc4bfad061d9fedae02bb726706b10ba
robin-epicswan <40164896+robin-epicswan@users.noreply.github.com>
2022-12-12 15:33:18 +0100

    Update hash.js

--- hash-secret-key/hash-nodeJS/hash.js
+++ hash-secret-key/hash-nodeJS/hash.js
@@ -1,4 +1,4 @@
-import crypto from 'crypto';
+
 function hashSecretKey(secretKey, serviceId) {
     const timestamp = Math.floor(Date.now() / 1000);
     const hash = crypto.createHmac('sha256', secretKey).update(serviceId + timestamp).digest('hex');
