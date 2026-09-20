1add17189721efba263db7643bf09eb21949a9e9
robin-epicswan <40164896+robin-epicswan@users.noreply.github.com>
2026-09-20 11:14:13 +0200

    CardFormat : ajout du format carte carrée (square) et dimensions à jour

--- php-class/v2/src/Enum/CardFormat.php
+++ php-class/v2/src/Enum/CardFormat.php
@@ -4,22 +4,29 @@ declare(strict_types=1);
 
 namespace MerciFacteur\Api\Enum;
 
-/** Format d'une carte illustrée. */
+/**
+ * Format d'une carte illustrée (papier 350 g/m²).
+ *
+ * Les dimensions sont celles de la carte après impression et coupe.
+ */
 enum CardFormat: string
 {
-    /** Carte postale sans enveloppe, 11 x 16 cm. */
+    /** Carte postale sans enveloppe, 10,5 x 15 cm. */
     case CARTE_POSTALE_NUE = 'naked-postcard';
 
-    /** Carte postale avec enveloppe, 11 x 16 cm. */
+    /** Carte postale avec enveloppe, 10,5 x 15 cm. */
     case CARTE_POSTALE = 'postcard';
 
-    /** Carte pliée, 15 x 21 cm fermée. */
+    /** Carte pliée, 14 x 19,5 cm fermée (28 x 19,5 ouverte). */
     case PLIEE = 'folded';
 
-    /** Carte non pliée, 15 x 21 cm. */
+    /** Carte non pliée, 14 x 19,5 cm. */
     case CLASSIQUE = 'classic';
 
-    /** Carte géante pliée, 19 x 21 cm fermée. */
+    /** Carte carrée, 14 x 14 cm. */
+    case CARREE = 'square';
+
+    /** Carte géante pliée, 18,5 x 26 cm fermée (37 x 26 ouverte). */
     case GEANTE = 'large';
 
     /** Carte géante A4, 21 x 29,7 cm. */
